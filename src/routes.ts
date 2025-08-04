8beea9f8bb3775ebebc0a60f7ebc77347b779236
shekar.ya <shekar.ya@assettl.com>
2025-08-04 10:56:54 +0530

    Adding Routes.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createEntity, createOrganisation, getLegalEntity, getLegalEntities, getOrganisation, getOrganisations, updateEntity, updateOrganisation } from './controllers/organisation';
+import { createEntity, createOrganisation, getLegalEntity, getLegalEntities, getOrganisation, getOrganisations, updateEntity, updateOrganisation, deleteLegalEntities, createLocation, updateLocation, deleteLocation, createDepartment, updateDepartment, deleteDepartment, getDepartments, getDepartment } from './controllers/organisation';
 
 const router = Router();
 
@@ -11,9 +11,24 @@ router.get('/getOrganisation/:id', getOrganisation)
 
 
 router.post('/createEntity', createEntity)
-router.put('/updateEntity/:id', updateEntity)
+router.put('/updateEntity', updateEntity)
 router.get('/getLegalEntities', getLegalEntities)
 router.get('/getLegalEntity/:id', getLegalEntity)
+router.delete('/deleteLegalEntities', deleteLegalEntities)
+
+
+router.post('/createLocation', createLocation)
+router.put('/updateLocation', updateLocation)
+// router.get('/getLocations', getLocation)
+// router.get('/getLocation/:id', getLocation)
+router.delete('/deleteLocation', deleteLocation)
+
+
+router.post('/createDepartment', createDepartment)
+router.put('/updateDepartment', updateDepartment)
+router.get('/getDepartments', getDepartments)
+router.get('/getDepartment/:id', getDepartment)
+router.delete('/deleteDepartment', deleteDepartment)
 
 
 
